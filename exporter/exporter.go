acf43e6b9517e139f6411679f6721dda95afd281
Ludovic Houdayer <ludovic.houdayer@fr.clara.net>
2023-02-08 13:51:25 +0100

    feat: adding go routines to fetch data

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -2,6 +2,7 @@ package exporter
 
 import (
 	"strconv"
+	"sync"
 
 	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
@@ -19,6 +20,7 @@ func NewExporter(saltUrl string, saltUser string, saltPassword string) *Exporter
 	}
 }
 
+var wg sync.WaitGroup
 var masterUp = prometheus.NewDesc(prometheus.BuildFQName("saltstack", "", "master_up"), "Master in up(1) or down(0)", []string{"master"}, nil)
 var minionsCount = prometheus.NewDesc(prometheus.BuildFQName("saltstack", "", "minions_count"), "Number of minions declared in salt", nil, nil)
 var jobsStatus = prometheus.NewDesc(prometheus.BuildFQName("saltstack", "", "job_status"), "Job status", []string{"minion", "function"}, nil)
@@ -41,70 +43,43 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 		}).Fatal(err)
 	}
 
-	// Check master status
-	masters, err := f.Masters()
-	if err != nil {
-		log.WithFields(log.Fields{
-			"saltUrl":      e.saltUrl,
-			"saltUser":     e.saltPassword,
-			"saltPassword": "***",
-		}).Error(err)
-	}
+	// Create all chanels needed for the go routines
+	masterChan := make(chan Masters)
+	minionsChan := make(chan Minions)
+	jobsChan := make(chan []Job)
 
-	for k, v := range masters.status {
+	// Go routine on all simple data fetcher
+	go f.Masters(masterChan)
+	go f.Minions(minionsChan)
+	go f.Jobs(jobsChan)
+
+	// Treat data from channels
+	for k, v := range (<-masterChan).status {
 		ch <- prometheus.MustNewConstMetric(masterUp, prometheus.GaugeValue, map[bool]float64{true: 1, false: 0}[v], k)
 	}
 
-	// Check minions status
-	minions, err := f.Minions()
-	if err != nil {
-		log.WithFields(log.Fields{
-			"saltUrl":      e.saltUrl,
-			"saltUser":     e.saltPassword,
-			"saltPassword": "***",
-		}).Error(err)
-	}
-	ch <- prometheus.MustNewConstMetric(minionsCount, prometheus.GaugeValue, float64(minions.count))
+	ch <- prometheus.MustNewConstMetric(minionsCount, prometheus.GaugeValue, float64((<-minionsChan).count))
 
 	// Check jobs status
 	// Status to be checked : state.highstate / state.apply with Arguments = []
 	// For one given Job check the Minions list and the Result for this minion
-	jobs, err := f.Jobs()
-	if err != nil {
-		log.WithFields(log.Fields{
-			"saltUrl":      e.saltUrl,
-			"saltUser":     e.saltPassword,
-			"saltPassword": "***",
-		}).Error(err)
-	}
+	var jobs_details []JobStatus
+	jobsStatusChan := make(chan JobStatus, 1000)
+	minion_last_event := make(map[string]uint64)
 
-	var jobs_details []*JobStatus
-	for _, job := range *jobs {
+	for _, job := range <-jobsChan {
 		if job.function == "state.highstate" || job.function == "state.apply" {
-			// store the job ID if job.function and job.id and job.target
-			job_status, err := f.JobStatus(job.id)
-			if err != nil {
-				log.WithFields(log.Fields{
-					"saltUrl":      e.saltUrl,
-					"saltUser":     e.saltPassword,
-					"saltPassword": "***",
-				}).Error(err)
-			}
-
-			jobs_details = append(jobs_details, job_status)
-
-			/*for _, minion := range job_status.minions {
-				var status bool = job_status.status[minion]
-				// var retcode float64 = job_status.errors[minion]
-
-				ch <- prometheus.NewMetricWithTimestamp(job_status.start_time,
-						prometheus.MustNewConstMetric(jobsStatus, prometheus.GaugeValue, map[bool]float64{true: 1, false: 0}[status], minion, job_status.function))
-
-			}*/
+			wg.Add(1)
+			go f.JobStatus(job.id, jobsStatusChan)
 		}
 	}
+	wg.Wait()
+	close(jobsStatusChan)
 
-	minion_last_event := make(map[string]uint64)
+	for elem := range jobsStatusChan {
+		jobs_details = append(jobs_details, elem)
+	}
+
 	for _, detail := range jobs_details {
 		for _, minion := range detail.minions {
 			id, _ := strconv.ParseUint(detail.id[0:14], 10, 64)
@@ -113,8 +88,6 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 			}
 		}
 	}
-
-	log.Debug(minion_last_event)
 
 	for _, detail := range jobs_details {
 		for _, minion := range detail.minions {
@@ -127,5 +100,4 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 			}
 		}
 	}
-
 }
